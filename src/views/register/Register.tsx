95320d79f6c108162689d94f10ac7cd662c885eb
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-08-07 17:37:17 +0200

    Create registration form

--- src/views/register/Register.tsx
+++ src/views/register/Register.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import "./styles.scss";
 
 type Props = {};
 
 const Register = (props: Props) => {
-  return <div>Register</div>;
+  return (
+    <div className="registerWrapper">
+      <h2 className="registerWrapper__header">Register</h2>
+      <form className="registerForm">
+        <label className="registerForm__label" htmlFor="username">
+          name
+        </label>
+        <input className="registerForm__input" name="username" id="username" />
+        <label className="registerForm__label" htmlFor="email">
+          e-mail
+        </label>
+        <input
+          className="registerForm__input"
+          type="email"
+          name="email"
+          id="email"
+        />
+        <label className="registerForm__label" htmlFor="password">
+          password
+        </label>
+        <input
+          className="registerForm__input"
+          type="password"
+          name="password"
+          id="password"
+        />
+        <label className="registerForm__label" htmlFor="confirmPassword">
+          confirm password
+        </label>
+        <input
+          className="registerForm__input"
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Register;
