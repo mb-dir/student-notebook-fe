4fa77298d542c02b015a1d9f5393404747f97528
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-08-07 17:24:51 +0200

    Create login form

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import "./styles.scss";
 
 type Props = {};
 
 const Login = (props: Props) => {
-  return <div>Login</div>;
+  return (
+    <div className="loginWrapper">
+      <h2 className="loginWrapper__header">Login</h2>
+      <form className="loginForm">
+        <label className="loginForm__label" htmlFor="email">
+          e-mail
+        </label>
+        <input
+          className="loginForm__input"
+          type="email"
+          name="email"
+          id="email"
+        />
+
+        <label className="loginForm__label" htmlFor="password">
+          password
+        </label>
+        <input
+          className="loginForm__input"
+          type="password"
+          name="password"
+          id="password"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
