b3f91d69a43e9263a159844c847f4b8e6fac6fbe
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-09-03 17:47:32 +0200

    Add collapse component to notes dashboard

--- src/views/notesDashboard/NotesDashboard.tsx
+++ src/views/notesDashboard/NotesDashboard.tsx
@@ -1,7 +1,45 @@
-import { FC } from "react";
+import "./styles.scss";
+
+import { FC, useState } from "react";
+
+import { Collapse } from "react-collapse";
 
 const NotesDashboard: FC = () => {
-  return <div>NotesDashboard</div>;
+  const [isAddNewNoteOpen, setIsAddNewNoteOpen] = useState<boolean>(false);
+  const [isShowAllNotesOpen, setIsShowAllNotesOpen] = useState<boolean>(false);
+  const onAddNewNoteClick = () => {
+    setIsAddNewNoteOpen(prev => !prev);
+    setIsShowAllNotesOpen(false);
+  };
+  const onShowAllNotesClick = () => {
+    setIsShowAllNotesOpen(prev => !prev);
+    setIsAddNewNoteOpen(false);
+  };
+  return (
+    <div>
+      <h2 className="notesHeader">
+        Here you can manage all your notes and add new one as well!
+      </h2>
+      <div className="collapseWrapper">
+        <div className="collapseWrapper__buttons">
+          <button
+            onClick={onAddNewNoteClick}
+            className="collapseWrapper__optionButton"
+          >
+            Add new note
+          </button>
+          <button
+            onClick={onShowAllNotesClick}
+            className="collapseWrapper__optionButton"
+          >
+            Show all notes
+          </button>
+        </div>
+        <Collapse isOpened={isAddNewNoteOpen}>Add new note</Collapse>
+        <Collapse isOpened={isShowAllNotesOpen}>Show all notes</Collapse>
+      </div>
+    </div>
+  );
 };
 
 export default NotesDashboard;
