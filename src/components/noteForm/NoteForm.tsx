d900d6774573aa81bbff638b7cef08b61420d30c
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-09-04 20:38:10 +0200

    Make all intputs controlled

--- src/components/noteForm/NoteForm.tsx
+++ src/components/noteForm/NoteForm.tsx
@@ -1,21 +1,42 @@
 import "./styles.scss";
 
+import { SubmitHandler, useForm } from "react-hook-form";
+
 import { FC } from "react";
 
+interface IFormInput {
+  title: string;
+  content: string;
+  isHighPriority: boolean;
+}
+
 const NoteForm: FC = () => {
+  const { register, handleSubmit } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = ({
+    title,
+    content,
+    isHighPriority,
+  }) => {
+    console.log(title, content, isHighPriority);
+  };
   return (
-    <form className="noteForm">
+    <form className="noteForm" onSubmit={handleSubmit(onSubmit)}>
       <div className="noteForm__inputWrapper">
         <label className="noteForm__label" htmlFor="title">
           Title
         </label>
-        <input className="noteForm__input" id="title" />
+        <input className="noteForm__input" id="title" {...register("title")} />
       </div>
       <div className="noteForm__inputWrapper">
         <label className="noteForm__label" htmlFor="content">
           Content
         </label>
-        <textarea className="noteForm__textarea" id="content" />
+        <textarea
+          className="noteForm__textarea"
+          id="content"
+          {...register("content")}
+        />
       </div>
       <div className="noteForm__inputWrapper">
         <label className="noteForm__label" htmlFor="isHighPriority">
@@ -25,6 +46,7 @@ const NoteForm: FC = () => {
           type="checkbox"
           className="noteForm__checkbox"
           id="isHighPriority"
+          {...register("isHighPriority")}
         />
       </div>
       <button className="noteForm__button">Add</button>
