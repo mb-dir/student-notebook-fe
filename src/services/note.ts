e2c1a3f6fafff1757e068126ce82ac5e2ec3cc49
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-09-06 18:30:17 +0200

    Function for getting all notes

--- src/services/note.ts
+++ src/services/note.ts
@@ -1,12 +1,25 @@
 import axiosInstance from "../axiosInstance";
 
-type Note = {
+export type Note = {
   title: string;
   content: string;
   isHighPriority: boolean;
 };
+
+export type NoteData = {
+  notes: Note[];
+  totalNotesCount: number;
+  page: number;
+  notesPerPage: number;
+  notesOnCurrentPage: number;
+};
 export const addNote = async (noteData: Note) => {
   const { data } = await axiosInstance.post<Note>("/notes", noteData);
 
+  return data;
+};
+
+export const getNotes = async () => {
+  const data = await axiosInstance.get<NoteData>("/notes");
   return data;
 };
