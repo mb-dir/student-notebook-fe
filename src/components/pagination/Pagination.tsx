821b6712c9b3559c6c523ace003074ff7af9032a
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-09-18 17:49:12 +0200

    Update URL when user uses pagination

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -2,6 +2,8 @@ import "./styles.scss";
 
 import { Dispatch, FC, SetStateAction } from "react";
 
+import { useNavigate } from "react-router-dom";
+
 type PaginationProps = {
   page: number;
   totalNotesCount: number;
@@ -15,13 +17,19 @@ const Pagination: FC<PaginationProps> = ({
   notesPerPage,
   setPaginationPage,
 }) => {
+  const navigate = useNavigate();
+
   const paginationButtonsAmount = Math.ceil(totalNotesCount / notesPerPage);
   const buttons = [];
+
   for (let i = 1; i <= paginationButtonsAmount; i++) {
     buttons.push(
       <button
         key={i}
-        onClick={() => setPaginationPage(i)}
+        onClick={() => {
+          setPaginationPage(i);
+          navigate(`/notes?page=${i}`);
+        }}
         className={`paginationWrapper__button ${
           page === i ? "paginationWrapper__button--active" : ""
         }`}
