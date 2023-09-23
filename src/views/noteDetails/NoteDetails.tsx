73db948841d39821f41cf66626617be8ee825237
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-09-23 21:19:27 +0200

    Basic view for note

--- src/views/noteDetails/NoteDetails.tsx
+++ src/views/noteDetails/NoteDetails.tsx
@@ -1,11 +1,47 @@
 import "./styles.scss";
 
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import { Note, getNote } from "../../services/note";
+
+import { toast } from "react-toastify";
 import { useParams } from "react-router";
 
 const NoteDetails: FC = () => {
   const { noteId } = useParams();
-  return <div>{noteId}</div>;
+  const [noteData, setNoteData] = useState<Note | null>(null);
+
+  useEffect(() => {
+    const getSingleNote = async () => {
+      try {
+        if (!noteId) {
+          toast.error(
+            "An issue has occurred; please attempt the action again or get in touch with the administrator for assistance."
+          );
+          return;
+        }
+        const data = await getNote(noteId);
+        setNoteData(data);
+      } catch (error: any) {
+        toast.error(error.response.data.error);
+      }
+    };
+    getSingleNote();
+  }, [noteId]);
+
+  return (
+    // React Quill here
+    <div className="noteWrapper">
+      {noteData ? (
+        <>
+          <h2>{noteData.title}</h2>
+          <p>{noteData.content}</p>
+          <p>Priority: {noteData.isHighPriority ? "High" : "Low"}</p>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 
 export default NoteDetails;
