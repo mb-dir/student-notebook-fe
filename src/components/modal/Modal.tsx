16c850ffddc3c935278ab8b1e3803b72ff80df28
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-09-24 22:30:22 +0200

    Close modal when click outside modal children

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,15 +1,38 @@
 import "./styles.scss";
 
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useEffect, useRef } from "react";
 
 type ModalProps = {
   children: ReactNode;
+  onClose: () => void;
 };
 
-const Modal: FC<ModalProps> = ({ children }) => {
+const Modal: FC<ModalProps> = ({ children, onClose }) => {
+  const modalRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      if (e.target === modalRef.current) onClose();
+    };
+
+    const modalElement = modalRef.current;
+
+    if (modalElement) {
+      modalElement.addEventListener("click", handleOutsideClick);
+    }
+
+    return () => {
+      if (modalElement) {
+        modalElement.removeEventListener("click", handleOutsideClick);
+      }
+    };
+  }, [onClose]);
+
   return (
     <div className="modal">
-      <div className="modal__body">{children}</div>
+      <div className="modal__body" ref={modalRef}>
+        {children}
+      </div>
     </div>
   );
 };
