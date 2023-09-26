ff3983feba491c44fc8a6fda4da65b0ba3ba1271
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-09-26 15:36:23 +0200

    Rid of redundant styles

--- src/components/confirmPopup/ConfirmPopup.tsx
+++ src/components/confirmPopup/ConfirmPopup.tsx
@@ -11,10 +11,16 @@ const ConfirmPopup: FC<ConfirmPopupProps> = ({ onConfirm, onReject }) => {
   return (
     <div className="confirmPopup">
       <p>Are you sure you want to do this?</p>
-      <button className="confirmPopup__confirm" onClick={onConfirm}>
+      <button
+        className="confirmPopup__button confirmPopup__button--confirm"
+        onClick={onConfirm}
+      >
         Yes
       </button>
-      <button className="confirmPopup__reject" onClick={onReject}>
+      <button
+        className="confirmPopup__button confirmPopup__button--reject"
+        onClick={onReject}
+      >
         No
       </button>
     </div>
