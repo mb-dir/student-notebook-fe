ccf929fd572c5a978dfdbb8fe3e650c8fcfd85d6
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-11-27 19:32:39 +0100

    Plan what have to be done

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -6,6 +6,7 @@ import NotesDashboard from "../views/notesDashboard/NotesDashboard";
 import UserDashboard from "../views/userDashboard/UserDashboard";
 
 export const PrivateRoutes: FC = () => {
+  // Right now we check token only when there is request, we need to do that on each route, cuz when user left the app on page where there is no communication with api, we cannot check if token expired or not
   return (
     <Routes>
       <Route path="/" element={<UserDashboard />} />
