b963bbe216277516aed9d8a0b79527afd67ab75d
Michał Bysiewcz <michal.bysiewicz.kontakt@gmail.com>
2024-02-15 18:11:29 +0100

    Routing for todos

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -4,6 +4,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import NoteDetails from "../views/noteDetails/NoteDetails";
 import NotesDashboard from "../views/notesDashboard/NotesDashboard";
 import UserDashboard from "../views/userDashboard/UserDashboard";
+import TodosDashboard from "../views/todosDashboard/TodosDashboard";
 
 export const PrivateRoutes: FC = () => {
   return (
@@ -12,6 +13,9 @@ export const PrivateRoutes: FC = () => {
       <Route path="notes">
         <Route index element={<NotesDashboard />} />
         <Route path=":noteId" element={<NoteDetails />} />
+      </Route>
+      <Route path="todos">
+        <Route index element={<TodosDashboard />} />
       </Route>
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
---
+++ src/views/todosDashboard/TodosDashboard.tsx
@@ -0,0 +1,9 @@
+import "./styles.scss";
+
+import { FC } from "react";
+
+const TodosDashboard: FC = () => {
+  return <h1>całete</h1>;
+};
+
+export default TodosDashboard;
